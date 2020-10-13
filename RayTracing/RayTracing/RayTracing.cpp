72595bfe931d89230f1826ee10d337a14c2c1df7
moss_studio <wangxc1998@qq.com>
2020-10-13 16:05:10 +0800

    Chapter12

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -77,25 +77,16 @@ vec3 color(const ray& r, hitable *world, int depth) {
 
 int main()
 {
-	int nx = 200;
-	int ny = 100;
-	int ns = 100;	// 采样次数
+	int nx = 1200;
+	int ny = 800;
+	int ns = 10;	// 采样次数
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	hitable *list[5];
-	float R = cos(M_PI / 4);
-	vec3 lookfrom(3, 3, 2);
-	vec3 lookat(0, 0, -1);
-	float dist_to_focus = (lookfrom - lookat).length();
-	float aperture = 2.0;
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	float dist_to_focus = 10.0;
+	float aperture = 0.1;
 
-	//list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
-	//list[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
-	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
-	list[1] = new sphere(vec3(0, -100.5, -1), 100,new lambertian(vec3(0.8,0.8,0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2),0.0));
-	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-	hitable *world = new hitable_list(list, 5);
+	hitable *world = random_scene();
 	
 	camera cam = camera(lookfrom,lookat,vec3(0,1,0),20,float(nx)/float(ny),aperture,dist_to_focus);
 	for (int j = ny - 1; j >= 0; j--)
@@ -108,7 +99,6 @@ int main()
 				float u = float(i + randomZeroToOne()) / float(nx);	// 此处写法为了规避rand函数伪随机的问题
 				float v = float(j + randomZeroToOne()) / float(ny);
 				ray r = cam.get_ray(u, v);
-				vec3 p = r.point_at_parameter(2.0);
 				col += color(r, world, 0);
 			}
 			col /= float(ns);
@@ -121,4 +111,3 @@ int main()
 		}
 	}
 }
-
