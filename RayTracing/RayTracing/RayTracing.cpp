2d2020cd91901cc47503a2868df8c6fd4d0b956f
moss_studio <wangxc1998@qq.com>
2020-10-13 14:49:26 +0800

    Chapter10

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -54,6 +54,9 @@ int main()
 	int ns = 100;	// 采样次数
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 	hitable *list[5];
+	float R = cos(M_PI / 4);
+	//list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
+	//list[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
 	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100,new lambertian(vec3(0.8,0.8,0.0)));
 	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2),0.0));
@@ -61,7 +64,7 @@ int main()
 	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
 	hitable *world = new hitable_list(list, 5);
 	
-	camera cam;
+	camera cam = camera(vec3(-2,2,1),vec3(0,0,-1),vec3(0,1,0),90,float(nx)/float(ny));
 	for (int j = ny - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
